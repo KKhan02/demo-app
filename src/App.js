532101fa3a7a24e214e2a167f55749f24de679e0
Khuzaim Khan <khuzaimkhan123@gmail.com>
2020-06-12 20:34:10 +0500

    Asssignment(9-June)
    Created Assignment of 9th June 2020. Temperature and Bulb Hook counter thingy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Room from './Room';
+import Bulb from './Bulb';
+import './Bulb.css';
 
 function App() {
   return (
     <div>
-      <Room />
+      {/* <Room /> */}
+      <br/>
+      <Bulb />
     </div>
   );
 }
---
+++ src/Bulb.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './Bulb.css';
+import logo from './idea-512.png';
+import './thermometer.css';
+
+
+function Bulb() {
+   const [isLit, setLit] = useState(true);
+   let [temperature,setTemperature] = useState(22);
+    
+
+
+  return (
+    <div>
+    <div className={`Bulb ${isLit ? "lit" : "dark"}`} >
+      
+        <div className="Bulb">
+          {/* <i class="fas fa-lightbulb"></i> */}
+          <img src={logo}/>    
+        </div>
+        <button className="ON"
+        onClick={()=>{
+          console.log("ON");
+          if (isLit === false) setLit(!isLit)}}> ON</button> 
+        <button className="OFF"
+                 onClick={()=>{
+                  console.log("OFF");
+                  if (isLit === true) setLit(!isLit)}}> OFF</button>    
+      </div>
+      <div className="Thermo">
+      <div className="doughnut">
+        <div className="inner-doughnut">
+            <div className="inner-inner-doughnut">
+            <button id="increment"
+            onClick={()=>{
+              console.log("increment");
+              setTemperature(++temperature);
+            }}><b>+</b></button>
+            <button id="decrement"
+            onClick={()=>{
+              console.log("decrement");
+              setTemperature(--temperature);
+            }}><b>-</b></button>
+            <p>
+                {temperature}°C
+            </p>
+        </div>
+        </div>
+                
+    </div>
+
+      </div>
+      </div>
+  );
+
+}
+export default Bulb;
